681524f15f7b823a5555adb3096239ba2558cebd
vaziliybober <vaziliybober@gmail.com>
2021-06-07 14:39:45 +0300

    fix theme ui deps issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,26 @@
 import React from 'react'
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { ThemeProvider } from 'theme-ui'
+import theme from './themes/theme'
 
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import HomePage from './components/HomePage/HomePage'
 import ArticlePage from './components/ArticlePage/ArticlePage'
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/article/:id">
-          <ArticlePage />
-        </Route>
-        <Route path="/">
-          <HomePage />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/article/:id">
+            <ArticlePage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   )
 }
 
